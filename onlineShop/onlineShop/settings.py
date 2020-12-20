bc5d636cd129d4caa2a0f37893deb49bdb640d46
alireza ryahi <34089243+alirezaryahi@users.noreply.github.com>
2020-12-20 12:11:40 +0330

    change database from postgresql to dbsqlite3

--- onlineShop/onlineShop/settings.py
+++ onlineShop/onlineShop/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'onlineShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'onlineshop',
-        'USER': 'postgres',
-        'PASSWORD': '9110182',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
