b6ba7ad103dd93c67f9effe98ce1128bcdeca9c3
alireza ryahi <34089243+alirezaryahi@users.noreply.github.com>
2020-12-21 11:44:29 +0330

    add RetrieveDestroyAPIView, RetrieveUpdateAPIView

--- onlineShop/Calture/api/views.py
+++ onlineShop/Calture/api/views.py
@@ -1,44 +1,43 @@
 from rest_framework.views import APIView
+from django.db.models import Q
 from rest_framework import generics, status
 from .serializers import CaltureSerializer
 from Calture.models import Effect
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 
 class AllCalture(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Effect.objects.all()
     serializer_class = CaltureSerializer
+        def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            queryset = Effect.objects.filter(
+                Q(subject__title__icontains=query) |
+                Q(title__icontains=query) |
+                Q(description__icontains=query+' ')
+            )
+            return queryset
+        else:
+            return Effect.objects.all()
 
 
-class UpdateCalture(generics.UpdateAPIView):
+class UpdateCalture(generics.RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
-
-    def get(self, request, pk):
-        query = Effect.objects.get(pk=pk)
-        serializers = CaltureSerializer(query)
-        return Response(serializers.data, status=status.HTTP_200_OK)
-
     queryset = Effect.objects.all()
     serializer_class = CaltureSerializer
     lookup_field = 'pk'
 
 
-class DeleteCalture(generics.DestroyAPIView):
+class DeleteCalture(generics.RetrieveDestroyAPIView):
     permission_classes = [IsAuthenticated]
-
-    def get(self, request, pk):
-        query = Effect.objects.get(pk=pk)
-        serializers = CaltureSerializer(query)
-        return Response(serializers.data, status=status.HTTP_200_OK)
-
     queryset = Effect.objects.all()
     serializer_class = CaltureSerializer
     lookup_field = 'pk'
 
 
 class PostCalture(generics.CreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
     queryset = Effect.objects.all()
     serializer_class = CaltureSerializer
